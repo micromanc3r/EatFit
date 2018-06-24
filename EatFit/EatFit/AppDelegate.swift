9d4904f47b1a2755ea8bb3c3e25628b888c30ba6
micromanc3r <micromanc3r@gmail.com>
2018-06-24 23:34:30 +0200

    Setup loglevel
    Set loglevel for debug to .verbose and .error for production.

--- EatFit/EatFit/AppDelegate.swift
+++ EatFit/EatFit/AppDelegate.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2018 micromanc3r. All rights reserved.
 //
 
+import MicroLogger
 import UIKit
 
 @UIApplicationMain
@@ -13,6 +14,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        setUpLogs()
+
         return true
+    }
+
+    func setUpLogs() {
+        #if DEBUG
+            MLogger.logLevel = .verbose
+        #else
+            MLogger.logLevel = .error
+        #endif
     }
 }
