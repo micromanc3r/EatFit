b4cac0f6ad35f13115b4aa8a0ec41a36654701bf
micromanc3r <micromanc3r@gmail.com>
2018-06-27 21:59:34 +0200

    MealModel
    Create model for Meal, that consists of MealComponents

---
+++ EatFit/EatFit/Model/Meal.swift
@@ -0,0 +1,22 @@
+//
+//  Meal.swift
+//  EatFit
+//
+//  Created by micromanc3r on 27/06/2018.
+//  Copyright © 2018 micromanc3r. All rights reserved.
+//
+
+import Foundation
+
+struct Meal {
+    let cereal: Cereal
+    let protein: Protein
+    let fruit: Fruit
+    let vegetable: Vegetable
+    let dairy: Dairy
+    let fat: Fat
+    
+    var components: [MealComponent] {
+        return [cereal, protein, fruit, vegetable, dairy, fat]
+    }
+}
