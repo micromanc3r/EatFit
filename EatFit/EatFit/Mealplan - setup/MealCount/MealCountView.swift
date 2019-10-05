d1e34ceb392db694b77b3d3d0f72fdbe6850c435
micromanc3r <micromanc3r@gmail.com>
2019-10-05 16:36:11 +0200

    wip: before migration

---
+++ EatFit/EatFit/Mealplan - setup/MealCount/MealCountView.swift
@@ -0,0 +1,21 @@
+//
+//  MealCountView.swift
+//  EatFit
+//
+//  Created by micromanc3r on 28/09/2019.
+//  Copyright © 2019 micromanc3r. All rights reserved.
+//
+
+import SwiftUI
+
+struct MealCountView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MealCountView_Previews: PreviewProvider {
+    static var previews: some View {
+        MealCountView()
+    }
+}
